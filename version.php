560fa3d2d8dc8d7b78a28b12dc34a70f22fd9320
RayanSellou <126617401+RayanSellou@users.noreply.github.com>
2025-05-02 10:45:16 +0200

    .

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024062400;
+$plugin->version   = 2025050200;
 $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '4.4';
