d8211fd87370cd19f931cf26938fad3dfe8ef66f
RayanSellou <126617401+RayanSellou@users.noreply.github.com>
2025-06-06 09:09:48 +0200

    css inline block

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025060521;
+$plugin->version   = 2025060601;
 $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '4.4';
